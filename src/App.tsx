01704ea07df3c6692a4c7c40b9f25ff474421d9f
João Cyrino <joaopedrocancado@gmail.com>
2022-03-07 21:51:38 -0300

    front nft

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import cyrinoNFTabi from "./CyrinoNFT.json";
 
-function App() {
+export default function App() {
+  const [toAddress, settoAddress] = useState<string>();
+  const [isOwner, setisOwner] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function load() {
+      // @ts-ignore
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const accounts = await provider.send("eth_requestAccounts", []) as string[];
+
+      const signer = await provider.getSigner();
+
+      const nftContract = new ethers.Contract('0x2A94dFc936Db88B1c28c639d4507A5c4ED36ed80', cyrinoNFTabi, signer);
+
+      const owner = await nftContract.getCollectionOwner();
+
+      if (owner === accounts[0]) {
+        setisOwner(true);
+      }
+    }
+
+    load();
+  }, []);
+
+  // @ts-ignore
+  const mint = async (e) => {
+    e.preventDefault();
+
+    // @ts-ignore
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts", []) as string[];
+
+    const signer = await provider.getSigner();
+
+    const nftContract = new ethers.Contract('0x2A94dFc936Db88B1c28c639d4507A5c4ED36ed80', cyrinoNFTabi, signer);
+
+    nftContract.mint(toAddress);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      width: '100vw',
+      height: '100vh',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      {isOwner ? <>
+        <input value={toAddress} onChange={e => settoAddress(e.target.value)} />
+        <button style={{ cursor: 'pointer' }} onClick={mint}>mint nft</button>
+      </>
+        :
+        <p>Only collections owner can interact with the contract</p>}
     </div>
   );
 }
-
-export default App;
